63f4d036223999305808429786e37cac44def3a3
Warrially <warrially@139.com>
2018-05-15 12:12:30 +0800

    农历

--- demo/main.go
+++ demo/main.go
@@ -1,13 +1,13 @@
 package main
 
 import (
-	"github.com/warrially/BaziGo"
-	"log"
+	_ "github.com/warrially/BaziGo/Lunar"
+	_ "log"
 )
 
 func main() {
 	// 年月日时分秒 性别(1男0女)
-	bazi := BaziGo.GetBazi(1986, 12, 22, 21, 30, 0, 1)
-	BaziGo.PrintBazi(bazi)
-	log.Println(bazi)
+	// bazi := BaziGo.GetBazi(1995, 12, 22, 21, 30, 0, 1)
+	// log.Println(bazi)
+	// BaziGo.PrintBazi(bazi)
 }
