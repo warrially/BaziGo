d2425e2629aff7bae04e68322cd4e195cb9ebdd8
Warrially <warrially@139.com>
2018-05-17 18:38:00 +0800

    增加闰月字符串

--- Common/define.go
+++ Common/define.go
@@ -279,3 +279,28 @@ func GetYinYangFromNumber(nValue int) string {
 	}
 	return ""
 }
+
+var LUNAR_MONTH_STR = [12]string{
+	"一月", "二月", "三月", "四月", "五月", "六月",
+	"七月", "八月", "九月", "十月", "冬月", "腊月"}
+
+func GetLunarMonthFromNumber(nValue int) string {
+	if (nValue >= 1) && (nValue <= 12) {
+		return LUNAR_MONTH_STR[nValue-1]
+	}
+
+	return ""
+}
+
+var LUNAR_DAY_STR = [30]string{
+	"初一", "初二", "初三", "初四", "初五", "初六", "初七", "初八", "初九", "初十",
+	"十一", "十二", "十三", "十四", "十五", "十六", "十七", "十八", "十九", "二十",
+	"廿一", "廿二", "廿三", "廿四", "廿五", "廿六", "廿七", "廿八", "廿九", "三十"}
+
+func GetLunarDayFromNumber(nValue int) string {
+	if (nValue >= 1) && (nValue <= 30) {
+		return LUNAR_DAY_STR[nValue-1]
+	}
+
+	return ""
+}
