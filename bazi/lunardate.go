12442d83826d69d3ece8cfc75f87be92380f559e
warrially <warrially@139.com>
2020-05-12 11:22:46 +0800

    lunardate

--- bazi/lunardate.go
+++ bazi/lunardate.go
@@ -865,31 +865,6 @@ func (self *TLunarDate) GetAllDays() int {
 
 }
 
-var LUNAR_MONTH_STR = [12]string{
-	"一月", "二月", "三月", "四月", "五月", "六月",
-	"七月", "八月", "九月", "十月", "冬月", "腊月"}
-
-func GetLunarMonthFromNumber(nValue int) string {
-	if (nValue >= 1) && (nValue <= 12) {
-		return LUNAR_MONTH_STR[nValue-1]
-	}
-
-	return ""
-}
-
-var LUNAR_DAY_STR = [30]string{
-	"初一", "初二", "初三", "初四", "初五", "初六", "初七", "初八", "初九", "初十",
-	"十一", "十二", "十三", "十四", "十五", "十六", "十七", "十八", "十九", "二十",
-	"廿一", "廿二", "廿三", "廿四", "廿五", "廿六", "廿七", "廿八", "廿九", "三十"}
-
-func GetLunarDayFromNumber(nValue int) string {
-	if (nValue >= 1) && (nValue <= 30) {
-		return LUNAR_DAY_STR[nValue-1]
-	}
-
-	return ""
-}
-
 func (self *TLunarDate) String() string {
 	strResult := fmt.Sprintf("农历: %d年", self.nYear)
 
@@ -924,7 +899,7 @@ func (self *TLunarDate) String() string {
 		strResult += "腊月"
 	}
 
-	switch {
+	switch self.nDay {
 	case 1:
 		strResult += "初一"
 	case 2:
