749223858ba1a3faf0d9398bc47e20bc164b850c
warrially <mail@wary.wang>
2021-07-31 11:47:30 +0800

    一个新的DEMO

---
+++ htmldemo/main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+
+	bazi "github.com/warrially/BaziGo"
+)
+
+type Myhandler struct{}
+
+// HTTP 版本
+
+func main() {
+	http.HandleFunc("/", IndexHandler)
+	http.ListenAndServe(":7890", nil)
+}
+func IndexHandler(w http.ResponseWriter, r *http.Request) {
+	pBazi := bazi.GetBazi(1995, 6, 16, 19, 7, 0, 0)
+	fmt.Fprintln(w, pBazi.ToHTML())
+}
