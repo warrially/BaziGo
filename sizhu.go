71fe983645ec9e43fe09065d9858831cbfea59ef
warrially <mail@wary.wang>
2021-08-06 11:17:23 +0800

    喜用神

--- sizhu.go
+++ sizhu.go
@@ -25,6 +25,7 @@ type TSiZhu struct {
 	// pHeHuaChong *THeHuaChong // 合化冲 这个暂时没写， 后面一定补。
 	pSolarDate *TSolarDate // 新历日期
 	pBaziDate  *TBaziDate  // 八字历日期
+	pXiYong    *TXiYong    // 喜用神
 }
 
 func (m *TSiZhu) init() *TSiZhu {
@@ -37,7 +38,8 @@ func (m *TSiZhu) init() *TSiZhu {
 	nYearGan := m.pYearZhu.setDayGan(nDayGan).genYearGanZhi(m.pBaziDate.Year()).Gan().Value()
 	// 通过年干支和八字月
 	m.pMonthZhu.setDayGan(nDayGan).genMonthGanZhi(m.pBaziDate.Month(), nYearGan)
-
+	// 生成喜用神数据
+	m.pXiYong = NewXiYong(m)
 	return m
 }
 
@@ -89,4 +91,9 @@ func (m *TSiZhu) DayZhu() *TZhu {
 // HourZhu 返回时柱
 func (m *TSiZhu) HourZhu() *TZhu {
 	return m.pHourZhu
+}
+
+// 喜用神和五行强度
+func (m *TSiZhu) XiYong() *TXiYong {
+	return m.pXiYong
 }
