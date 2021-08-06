42c225f72233c2c8c15a2229122bcc3ca607e2dd
warrially <mail@wary.wang>
2021-08-06 11:17:33 +0800

    demo 修改报错

--- htmldemo/main.go
+++ htmldemo/main.go
@@ -15,7 +15,7 @@ func main() {
 	http.HandleFunc("/", IndexHandler)
 	http.ListenAndServe(":7890", nil)
 }
-func IndexHandler(w http.ResponseWriter, r *http.Request) {
+func IndexHandler(w http.ResponseWriter, _ *http.Request) {
 	pBazi := bazi.GetBazi(1995, 6, 16, 19, 7, 0, 0)
 	fmt.Fprintln(w, pBazi.ToHTML())
 }
